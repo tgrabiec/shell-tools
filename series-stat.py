a5d8437c9140bbe81bca1d9b6cbb7add624f3bd1
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-05-28 12:51:48 +0200

    add series-stat.py

---
+++ series-stat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import numpy
+import sys
+
+if __name__ == "__main__":
+    values = list()
+    for line in sys.stdin.readlines():
+        values.append(float(line))
+    if not values:
+        print "No data"
+    else:
+        print "avg = %.2f" % numpy.mean(values)
+        print "stdev = %.2f" % numpy.std(values)
+        print "samples = %d" % len(values)
